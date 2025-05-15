3ec99b9bd6f5710ac58654a3cbcd322bc62e94ec
joydaniella_122140039 <145269321+jdaniella039@users.noreply.github.com>
2025-05-15 23:36:27 +0700

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-from waitress import serve
-from pemweb import main
-from pyramid.paster import get_app
-
-if __name__ == '__main__':
-    app = get_app('development.ini', 'main')
-    serve(app, host='127.0.0.1', port=6543)
